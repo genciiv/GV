f9bbb475b8a00b72fea59700d353a3cf441f5f40
G-code <30412971+genciiv@users.noreply.github.com>
2026-05-20 09:00:52 +0200

    Fillo projektin btn fixed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -25,7 +25,7 @@ function ContactForm() {
   };
 
   return (
-    <section className="contact-form-section" id="contact">
+    <section className="contact-form-section" id="contact-form">
       <div className="container">
         <div className="form-wrapper">
           <div className="section-title">
